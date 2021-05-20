fe4a61d6bd8409e4268c96c6a563f2291a72a73e
Selik@kone <konnehjonny@gmail.com>
2021-05-20 03:20:58 +0300

    commited the merge

--- minipro.py
+++ minipro.py
@@ -11,19 +11,17 @@ lname = input('please, what is you last last name?')
 print(lname + 'Thanks!')
 
 def addi_tion():
-<<<<<<< HEAD
       return 1+1
 
 
 def sub_tration():
-    return 2+2
-=======
+    
     return 4+5
 
 
 def sub_tration():
     return 5-4
->>>>>>> c2d1355b7c9decfc9651cf57bafa7ec12545ea1e
+
 
 def mult_iplication():
     return 5*4
